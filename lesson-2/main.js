b12e8b123929f86ba762ec3702d2427fd12aa0f8
Konstantin Iosifov <iosifov.konstantin@gmail.com>
2019-06-29 00:06:32 +0300

    Добавил второе задание

---
+++ lesson-2/main.js
@@ -0,0 +1,32 @@
+let money = 580000;
+    income = "Фриланс, Спорт";
+    addExpenses = "Ноутбук, Мышь, Печеньки";
+    deposit = true;
+    mission = 1000000;
+    period = 10;
+    budgetDay = money / 30;
+
+console.log(typeof money );
+console.log(typeof income );
+console.log(typeof deposit );
+console.log(income.length);
+console.log("Период " + period + " месяцев");
+console.log("Цель заработать " + mission + " долларов");
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
+console.log(money % 30);
+
+var num = 266219;
+var lastDigit = 1;
+var multOfAllDigits = 1;
+
+while(num != 0)
+{
+lastDigit= num % 10;
+multOfAllDigits = multOfAllDigits * lastDigit;
+num = parseInt(num/10);
+}
+document.write("Произведение чисел = ", multOfAllDigits + "\n");
+document.write("Возведение в степень полученного числа = ", multOfAllDigits ** 3 + "\n");
+document.write("Вывод первых двух цифр = ", String(multOfAllDigits ** 3).slice(0,2));
+
