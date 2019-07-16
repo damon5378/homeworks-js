509ce42e44891224216d591efdcac11e17078747
Konstantin Iosifov <iosifov.konstantin@gmail.com>
2019-07-16 17:46:54 +0300

    10

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -10,14 +10,16 @@ function DomElement(selector, height, width, bg, fontSize){
 let question = prompt('Введите . (точку) или # (решетку)');
 DomElement.prototype.createElement = function(){
     let newDom;
-    if(this.selector === '.'){
+    if(this.selector[0] === '.'){
         newDom = document.createElement('div');
         newDom.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
     } else if (this.selector === '#') {
         newDom = document.createElement('p');
         newDom.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
     }
+    document.body.appendChild(newDom);
 };
+
 let dom = new DomElement(question, '100', '100', 'red', '18');
 dom.createElement();
 console.log(dom);
